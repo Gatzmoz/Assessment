620ce291c8cbef9641f3d58483f83dba03c1feda
Dito Rifki Irawan <dito.122140153@student.itera.ac.id>
2024-06-27 21:51:59 +0700

    chore: Menambahkan user_id dan question_id ke fillable atribut

---
+++ app/Models/Answer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'answers';
+    protected $primaryKey = 'answer_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'question_id',
+        'answer_id',
+        'user_id',
+        'nilai_sifat',
+        'teks_jawaban',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+        ];
+    }
+
+    public function Question()
+    {
+        return $this->hasOne(Question::class, 'question_id', 'question_id');
+    }
+
+}
